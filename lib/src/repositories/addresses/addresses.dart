82e4661295edd4f194f52053e0f94448e80f108c
Kevin Segaud <kevin.segaud@canb.eu>
2022-06-19 17:11:23 +0200

    Get Storage Value for Address Fixes #5

--- lib/src/repositories/addresses/addresses.dart
+++ lib/src/repositories/addresses/addresses.dart
@@ -3,6 +3,7 @@ import 'package:elrond_gateway/src/response/addresses/get_address/get_address.da
 import 'package:elrond_gateway/src/response/addresses/get_address_balance/get_address_balance.dart';
 import 'package:elrond_gateway/src/response/addresses/get_address_nonce/get_address_nonce.dart';
 import 'package:elrond_gateway/src/response/addresses/get_address_transactions/get_address_transactions.dart';
+import 'package:elrond_gateway/src/response/addresses/get_storage_value_for_address/get_storage_value_for_address.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'addresses.g.dart';
@@ -24,4 +25,8 @@ abstract class Addresses {
   @GET('/address/{bech32Address}/transactions')
   Future<GetAddressTransactionsResponse> getAddressTransactions(
       @Path() String bech32Address);
+
+  @GET('/address/{bech32Address}/key/{key}')
+  Future<GetStorageValueForAddressResponse> getStorageValueForAddress(
+      @Path() String bech32Address, @Path() String key);
 }
