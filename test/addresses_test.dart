c5ddc7efaaca11447b5e37e339b118ede2b1a975
Kevin Segaud <kevin.segaud@canb.eu>
2022-06-19 17:00:21 +0200

    Get Address Transactions Fixes #4

--- test/addresses_test.dart
+++ test/addresses_test.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:elrond_gateway/src/response/addresses/get_address/get_address.dart';
 import 'package:elrond_gateway/src/response/addresses/get_address_balance/get_address_balance.dart';
 import 'package:elrond_gateway/src/response/addresses/get_address_nonce/get_address_nonce.dart';
+import 'package:elrond_gateway/src/response/addresses/get_address_transactions/get_address_transactions.dart';
 import 'package:elrond_gateway/src/response/response.dart';
 import 'package:test/test.dart';
 
@@ -67,6 +68,64 @@ void main() {
         GetAddressBalanceResponse.fromJson(json.decode(getAddressBalanceJson));
     expect(response.data, isNotNull);
     expect(response.data!.balance, equals('100000000000000000000'));
+    expect(response.error, isEmpty);
+    expect(response.code, equals(GatewayCode.successful));
+  });
+
+  test('get address transactions', () {
+    final getAddressBalanceJson = '''{
+  "data": {
+    "transactions": [
+      {
+        "hash": "1a3e...",
+        "fee": "10000000000000000",
+        "miniBlockHash": "9673...",
+        "nonce": 68,
+        "round": 33688,
+        "value": "1000000000000000000",
+        "receiver": "erd1...",
+        "sender": "erd1l453hd0gt5gzdp7czpuall8ggt2dcv5zwmfdf3sd3lguxseux2fsmsgldz",
+        "receiverShard": 0,
+        "senderShard": 0,
+        "gasPrice": 200000000000,
+        "gasLimit": 50000,
+        "gasUsed": 50000,
+        "data": "",
+        "signature": "ed75...",
+        "timestamp": 1591258128,
+        "status": "success",
+        "scResults": null
+      },
+      {
+        "hash": "d72d...",
+        "fee": "10000000000000000",
+        "miniBlockHash": "fd45...",
+        "nonce": 67,
+        "round": 27353,
+        "value": "100000000000000000000000000",
+        "receiver": "erd1...",
+        "sender": "erd1l453hd0gt5gzdp7czpuall8ggt2dcv5zwmfdf3sd3lguxseux2fsmsgldz",
+        "receiverShard": 1,
+        "senderShard": 0,
+        "gasPrice": 200000000000,
+        "gasLimit": 50000,
+        "gasUsed": 50000,
+        "data": "",
+        "signature": "bb98...",
+        "timestamp": 1591220142,
+        "status": "success",
+        "scResults": null
+      }
+    ]
+  },
+  "error": "",
+  "code": "successful"
+}''';
+
+    final response = GetAddressTransactionsResponse.fromJson(
+        json.decode(getAddressBalanceJson));
+    expect(response.data, isNotNull);
+    expect(response.data!.transactions.length, equals(2));
     expect(response.error, isEmpty);
     expect(response.code, equals(GatewayCode.successful));
   });
